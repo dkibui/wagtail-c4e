565446c86db95face5f8a0fe91e8d96e94ea8c9e
David Kibui <dkibui@outlook.com>
2023-10-21 17:58:51 +0300

    Worked on home page. Also reformatted code

--- tutorial/home/models.py
+++ tutorial/home/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 from wagtail.models import Page
 
 
 class HomePage(Page):
     templates = "home/home_page.html"
+    max_count = 1
 
     banner_title = models.CharField(max_length=255, blank=True, null=False)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        FieldPanel("banner_image"),
     ]
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
